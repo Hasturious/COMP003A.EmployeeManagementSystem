419b2aaaa23a75cb797310f29de0c454eea5c10b
Hasturious <jbster559@gmail.com>
2025-02-12 18:06:45 -0800

    Add project files.

---
+++ COMP003A.EmployeeManagementSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿// Author: Jean Bryant Figueroa
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Employee management system demonstrating OOP principles in C#
+
+namespace COMP003A.EmployeeManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
